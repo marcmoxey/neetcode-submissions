e6ac496e65d10b01c355645d720d93fc0eb2555e
Marc Moxey <moxeymarc@gmail.com>
2026-06-08 11:50:14 -0400

    Update: duplicate-integer - submission-1

--- Data Structures & Algorithms/duplicate-integer/submission-1.cs
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -1,20 +1,43 @@
-       public class Solution
-       {
-           public bool hasDuplicate(int[] nums)
-           {
-               for (int i = 0; i < nums.Length; i++) 
-               {
-                   for (int j = i + 1; j < nums.Length; j++) 
-                   {
-                       if (nums[i] == nums[j])
-                       {
-                           return true;
-                       }
-                   
-                   }
-               }
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+    //     // loop through array 
+    //     for(int i = 0; i < nums.Length; i++)
+    //     {
+    //        for(int j = i + 1; j < nums.Length; j++) 
+    //        {
+    //             if(nums[i] == nums[j]) {
+    //                 return true;
+    //             }
+    //        }
+    //     }
+    //     return false;
 
-               return false;
+        // sort array 
+        // Array.Sort(nums);
 
-           }
-       }
+        // // loop through array 
+        // for(int i = 0; i < nums.Length; i++ ) {
+        //     // checking for dupes 
+        //     if(nums[i] == nums[i-1]) 
+        //     {
+        //     return true;
+        //     }
+
+        // }
+
+        // return false; 
+
+        HashSet<int> newNums = new(); 
+
+        foreach(var num in nums) 
+        {
+            if(newNums.Contains(num)) 
+            {
+                 return true;
+            }
+            newNums.Add(num);
+        }
+         
+         return false;
+    }
+ }
